b71ed8b2958290de5dfa34ca39885f19b52e4456
sdotrobert <robert950017@gmail.com>
2023-08-20 23:18:39 +0800

    Footer done. Can expand to tablet screen size.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,6 +27,7 @@ export default function Footer() {
       <div className={styles["footer-left"]}>
         <div className={styles["fzt-logo-div"]}>
           <Image
+            className={styles["fozirtex-logo-icon"]}
             src={fzt_landscape_logo}
             alt="Fozirtex-logo"
             title="FozirTex"
@@ -35,64 +36,93 @@ export default function Footer() {
       </div>
 
       <div className={styles["footer-right"]}>
-        <div className={styles["cata-sections"]}> {/* 探索 */}
-          <div className={styles["cata-titles"]}>{ftrTitles[0]}</div>
-          <div className={styles["sub-links"]}>
-            <div>
-              <Link href="/">{ftrSubLinks[0]}</Link>
+        {/* cata group 1 */}
+        <div className={styles["cata-group"]}>
+          <div className={styles["cata-sections"]}>
+            {" "}
+            {/* 探索 */}
+            <div className={styles["cata-titles"]}>{ftrTitles[0]}</div>
+            <div className={styles["sub-links"]}>
+              <div>
+                <Link href="/">{ftrSubLinks[0]}</Link>
+              </div>
+              <div>
+                <Link href="/">{ftrSubLinks[1]}</Link>
+              </div>
+              <div>
+                <Link href="/">{ftrSubLinks[2]}</Link>
+              </div>
             </div>
-            <div>
-              <Link href="/">{ftrSubLinks[1]}</Link>
-            </div>
-            <div>
-              <Link href="/">{ftrSubLinks[2]}</Link>
+          </div>
+          <div className={styles["cata-sections"]}>
+            {" "}
+            {/* 協助 */}
+            <div className={styles["cata-titles"]}>{ftrTitles[1]}</div>
+            <div className={styles["sub-links"]}>
+              <div>
+                <Link href="/">{ftrSubLinks[3]}</Link>
+              </div>
+              <div>
+                <Link href="/">{ftrSubLinks[4]}</Link>
+              </div>
             </div>
           </div>
         </div>
-        <div className={styles["cata-sections"]}> {/* 協助 */}
-          <div className={styles["cata-titles"]}>{ftrTitles[1]}</div>
-          <div className={styles["sub-links"]}>
-            <div>
-              <Link href="/">{ftrSubLinks[3]}</Link>
-            </div>
-            <div>
-              <Link href="/">{ftrSubLinks[4]}</Link>
-            </div>
-          </div>
-        </div>
-        <div className={styles["cata-sections"]}> {/* 關於 */}
-          <div className={styles["cata-titles"]}>{ftrTitles[2]}</div>
-          <div className={styles["sub-links"]}>
-            <div>
-              <Link href="/">{ftrSubLinks[5]}</Link>
-            </div>
-            <div>
-              <Link href="/">{ftrSubLinks[6]}</Link>
+
+        {/* cata group 2 */}
+        <div className={styles["cata-group"]}>
+          <div className={styles["cata-sections"]}> {/* 關於 */}
+            <div className={styles["cata-titles"]}>{ftrTitles[2]}</div>
+            <div className={styles["sub-links"]}>
+              <div>
+                <Link href="/">{ftrSubLinks[5]}</Link>
+              </div>
+              <div>
+                <Link href="/">{ftrSubLinks[6]}</Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles["cata-sections"]}> {/* 聯絡我們 */}
-          <div className={styles["cata-titles"]}>{ftrTitles[3]}</div>
-          <div className={styles["sub-links"]}>
-            <div>
-              <Link href="/">
-                <Image src={fb_icon} alt="FozirTex Facebook" width={20} height={20} />
-              </Link>
-            </div>
-            <div>
-              <Link href="/">
-                <Image src={ins_icon} alt="FozirTex Instagram" width={20} height={20} />
-              </Link>
-            </div>
-            <div>
-              <Link href="/">
-                <Image src={x_icon} alt="FozirTex Email" width={20} height={20} />
-              </Link>
-            </div>
-            <div>
-              <Link href="/">
-                <Image src={email_icon} alt="FozirTex X" width={20} height={20} />
-              </Link>
+          <div
+            className={`${styles["cata-sections"]} ${styles["contact-us-section"]}`}
+          > {/* 聯絡我們 */}
+            <div className={styles["cata-titles"]}>{ftrTitles[3]}</div>
+            <div className={styles["sub-links"]}>
+              <div>
+                <Link href="/">
+                  <Image
+                    className={styles["contact-images"]}
+                    src={fb_icon}
+                    alt="FozirTex Facebook"
+                  />
+                </Link>
+              </div>
+              <div>
+                <Link href="/">
+                  <Image
+                    className={styles["contact-images"]}
+                    src={ins_icon}
+                    alt="FozirTex Instagram"
+                  />
+                </Link>
+              </div>
+              <div>
+                <Link href="/">
+                  <Image
+                    className={styles["contact-images"]}
+                    src={x_icon}
+                    alt="FozirTex Email"
+                  />
+                </Link>
+              </div>
+              <div>
+                <Link href="/">
+                  <Image
+                    className={styles["contact-images"]}
+                    src={email_icon}
+                    alt="FozirTex X"
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
