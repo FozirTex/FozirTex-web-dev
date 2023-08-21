03dde0ec16d38be583d21461188a7d6e2b21755a
sdotrobert <robert950017@gmail.com>
2023-08-21 16:23:37 +0800

    Navbar basically done. Could add animations on tool-btns.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,6 +5,7 @@ import styles from "@/styles/_NavBars.module.scss";
 import Image from "next/image";
 import fzt_logo from "../../public/FozirTex_logo.svg";
 import ham_icon from "../../public/hamberger.svg";
+import night_light_icon from "../../public/night-light.svg";
 import lan_switch_icon from "../../public/language-switcher.svg";
 import { useState } from "react";
 
@@ -33,30 +34,95 @@ export default function NavBar() {
     <>
       <header className={styles["header-body"]}>
         <Link href="/" className={styles["logo-link"]}>
-          <div className={styles["logo-border"]}>
-            <Image src={fzt_logo} alt={"FozirTex Logo"} />
+          <div className={styles["logo-icon-border"]}>
+            <Image
+              className={styles["logo-itself"]}
+              src={fzt_logo}
+              alt={"FozirTex Logo"}
+            />
           </div>
         </Link>
+
+        {/* Navigation links for larger screens */}
+        <nav className={styles["nav-links-md-or-greater"]}>
+          <div>
+            <Link className={styles["nav-main-links-large"]} href="/">
+              {menuNames[3]}
+            </Link>
+          </div>
+          <div>
+            <Link className={styles["nav-main-links-large"]} href="/about-us">
+              {menuNames[4]}
+            </Link>
+          </div>
+          <div>
+            <Link
+              className={styles["nav-main-links-large"]}
+              href="/startup-records"
+            >
+              {menuNames[5]}
+            </Link>
+          </div>
+        </nav>
+
+        {/* Hamberger toggle button for small screens */}
         <div className={styles["hamberger-toggle"]}>
           <button onClick={toggleNav} className={styles["ham-toggle-btn"]}>
             <Image src={ham_icon} alt="hamberger-icon" />
           </button>
         </div>
+
+        {/* Tool buttons */}
+        <div className={styles["tool-btns"]}>
+          <div className={styles["tool-theme-border"]}>
+            <button className={styles["theme-switch-btn"]}>
+              <Image
+                id={styles["theme-switch-icon"]}
+                src={night_light_icon}
+                alt="Theme switcher"
+                title="Switch Theme"
+                width={36}
+                height={42}
+              />
+            </button>
+          </div>
+          <div className={styles["tool-lan-border"]}>
+            <button className={styles["lan-switch-btn"]}>
+              <Image
+                id={styles["lan-switch-icon"]}
+                src={lan_switch_icon}
+                alt="lauguage switcher"
+                title="Switch language"
+                width={36}
+                height={42}
+              />
+            </button>
+          </div>
+          <div className={styles["login-btn-border"]}>
+            <button className={styles["login-btn"]}>登入</button>
+          </div>
+        </div>
       </header>
 
+      {/* Toggle menu bar */}
       <div
         className={`${styles["navs"]} ${isNavOpen ? styles["nav-open"] : ""}`}
       >
         <div className={styles["nav-links"]}>{menuNames[0]}</div>
         <div className={`${styles["nav-links"]} ${styles["lan-switch"]}`}>
           {menuNames[1]}
-          <Image
-            src={lan_switch_icon}
-            alt="lauguage switcher"
-            title="Switch language"
-            className={styles["lan-switch-icon"]}
-          />
+          <button className={styles["lan-switch-icon"]}>
+            <Image
+              src={lan_switch_icon}
+              alt="lauguage switcher"
+              title="Switch language"
+              width={30}
+              height={30}
+            />
+          </button>
         </div>
+
+        {/* Navigation section */}
         <div className={styles["nav-links"]}>
           <h2>{menuNames[2]}</h2>
           <hr className={`${styles["solid"]} ${styles["divider"]}`} />
@@ -73,6 +139,8 @@ export default function NavBar() {
           <Link href="/startup-records">{menuNames[5]}</Link>
           <hr className={`${styles["solid"]} ${styles["divider"]}`} />
         </div>
+
+        {/* Member section */}
         <div className={styles["nav-links"]}>
           <h2>{menuNames[6]}</h2>
           <hr className={`${styles["solid"]} ${styles["divider"]}`} />
