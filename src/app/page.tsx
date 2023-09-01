6aa446db2fc48bc3fc3ba47012947250b4bfb757
Chang Wei <changwei1006@gmail.com>
2023-09-02 01:46:17 +0800

    add download

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,28 @@ export default function Home() {
     <div className={styles.container}>
       <main>
         <div className={styles.banner}>
-          <div className={styles.banner_start}>
-            <h1><span>B</span>ad<span>X</span> App</h1>
-            <div className={styles.banner_start_summary}>Ea ea fugiat id proident. Cupidatat excepteur non ullamco fugiat cillum quis id aliquip pariatur duis aute commodo.</div>
-            <div className={styles.banner_start_download}>
-              <Image src={appstoreSVG} alt={'Download on the AppStore'} />
+          <div className={styles.banner_inner}>
+            <div className={styles.banner_start}>
+              <h1><span>B</span>ad<span>X</span> App</h1>
+              <div className={styles.banner_start_summary}>Ea ea fugiat id proident. Cupidatat excepteur non ullamco fugiat cillum quis id aliquip pariatur duis aute commodo.</div>
+              <div className={styles.banner_start_download}>
+                <Image src={appstoreSVG} alt={'Download on the AppStore'} />
+              </div>
+            </div>
+            <div className={styles.banner_end}>
+              <Image src={bannerSVG} alt={''} />
             </div>
           </div>
-          <div className={styles.banner_end}>
-            <Image src={bannerSVG} alt={''} />
+        </div>
+
+        <div className={styles.download}>
+          <div className={styles.download_text}>
+            立即免費使用 <span>B</span>ad<span>X</span>，<br/>
+            以最輕鬆的方式<br/>
+            管理、預約、享受羽球運動！
+          </div>
+          <div className={styles.download_button}>
+            <Image src={appstoreSVG} alt={'Download on the AppStore'} />
           </div>
         </div>
       </main>
